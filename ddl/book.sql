c12978ac6e34ffa9d1e10c77b956583235cb3095
Fernando Dantas <fernandodantas1996@gmail.com>
2026-02-05 14:15:13 -0300

    feat: improved ddl

--- ddl/book.sql
+++ ddl/book.sql
@@ -2,5 +2,6 @@ create table book (
     id integer primary key asc,
     title string not null,
     author string,
-    genre string
+    genre string,
+    filepath string
 );
--- ddl/reading-log.sql
+++ ddl/reading-log.sql
@@ -1,6 +1,9 @@
 create table reading_log (
     id integer primary key asc,
     id_book integer not null,
+    initial_page integer not null,
+    final_page integer not null,
     timestamp integer not null,
+    next_topic string,
     FOREIGN KEY(id_book) REFERENCES book(id)
 );
