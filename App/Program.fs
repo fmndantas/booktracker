bae4685bee8f36c62fcb213f450e5aa9deaab7b8
Fernando Dantas <fernandodantas1996@gmail.com>
2026-02-13 08:39:25 -0300

    feat: added debug for main

--- App/Program.fs
+++ App/Program.fs
@@ -1,4 +1,6 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+
 open Argu
 
 open App
@@ -7,20 +9,41 @@ open App
 let bookFolder = "/home/fernando/books"
 let connectionString = "DataSource=" + __SOURCE_DIRECTORY__ + "/../booktracker.db"
 
-// FSharp.Data.Sql.Common.QueryEvents.SqlQueryEvent
-// |> Event.add (printfn "Executing SQL: %O")
+let debug (message: string) =
+  // printfn "[DEBUG - %s]: %s" (DateTime.UtcNow.ToString "O") message
+  ()
 
+FSharp.Data.Sql.Common.QueryEvents.SqlQueryEvent
+|> Event.add (fun e -> debug (sprintf "Executing SQL: %O" e))
+
+let timer = Diagnostics.Stopwatch.StartNew()
 let w = Context.getWriteContext connectionString
 let r = Context.getReadContext connectionString
+timer.Stop()
 
+debug (sprintf "context.t = %d" timer.ElapsedMilliseconds)
+
+timer.Restart()
 let parser = ArgumentParser.Create<Parser.Arguments>(programName = "booktracker")
+timer.Stop()
+
+debug (sprintf "parser.t = %d" timer.ElapsedMilliseconds)
 
 [<EntryPoint>]
 let main argv =
+  timer.Restart()
   let result = parser.ParseCommandLine argv
+  timer.Stop()
+
+  debug (sprintf "parser.ParseCommandLine = %d" timer.ElapsedMilliseconds)
+
+  timer.Restart()
 
   if result.Contains Parser.Arguments.Get_Books then
+    timer.Restart()
     r |> Workflow.getBooks
+    timer.Stop()
+    printfn "workflow.t = %d" timer.ElapsedMilliseconds
 
   if result.Contains Parser.Arguments.Get_Logs_By_Book then
     r |> Workflow.getLastReadingLogsByBook
@@ -33,5 +56,9 @@ let main argv =
 
   if result.Contains Parser.Arguments.Continue_Last_Reading then
     r |> Workflow.continueLastReading
+
+  timer.Stop()
+
+  debug (sprintf "workflow = %d" timer.ElapsedMilliseconds)
 
   0
