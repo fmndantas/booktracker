c10aa17e83718d563284792d6cfc5840ca6276f5
Fernando Dantas <fernandodantas1996@gmail.com>
2026-02-13 18:18:39 -0300

    feat: added debug mode

--- App/Parser.fs
+++ App/Parser.fs
@@ -4,10 +4,11 @@ open Argu
 
 type Arguments =
   | [<AltCommandLine("-b")>] Get_Books
-  | [<AltCommandLine("-l")>] Get_Logs_By_Book
-  | Create_Book
-  | Log_Reading
+  | [<AltCommandLine("-gl")>] Get_Logs_By_Book
+  | [<AltCommandLine("-cb")>] Create_Book
+  | [<AltCommandLine("-l")>] Log_Reading
   | [<AltCommandLine("-c")>] Continue_Last_Reading
+  | Debug
 
   interface IArgParserTemplate with
     member s.Usage =
@@ -16,4 +17,5 @@ type Arguments =
       | Get_Logs_By_Book -> "show logs by book"
       | Create_Book -> "create a book"
       | Log_Reading -> "log reading for a book"
-      | Continue_Last_Reading -> "continue last reading"
+      | Continue_Last_Reading -> "continue last reading"      
+      | Debug -> "run application in debug mode"
--- App/Program.fs
+++ App/Program.fs
@@ -9,53 +9,48 @@ open App
 let bookFolder = "/home/fernando/books"
 let connectionString = "DataSource=" + __SOURCE_DIRECTORY__ + "/../booktracker.db"
 
+let w = Context.getWriteContext connectionString
+let r = Context.getReadContext connectionString
+
+let parser = ArgumentParser.Create<Parser.Arguments>(programName = "booktracker")
+
+let mutable isDebug = false
+
 let debug (message: string) =
-  // printfn "[DEBUG - %s]: %s" (DateTime.UtcNow.ToString "O") message
-  ()
+  if isDebug then
+    printfn "[DEBUG - %s]: %s" (DateTime.UtcNow.ToString "O") message
 
 FSharp.Data.Sql.Common.QueryEvents.SqlQueryEvent
 |> Event.add (fun e -> debug (sprintf "Executing SQL: %O" e))
 
-let timer = Diagnostics.Stopwatch.StartNew()
-let w = Context.getWriteContext connectionString
-let r = Context.getReadContext connectionString
-timer.Stop()
-
-debug (sprintf "context.t = %d" timer.ElapsedMilliseconds)
-
-timer.Restart()
-let parser = ArgumentParser.Create<Parser.Arguments>(programName = "booktracker")
-timer.Stop()
-
-debug (sprintf "parser.t = %d" timer.ElapsedMilliseconds)
-
 [<EntryPoint>]
 let main argv =
-  timer.Restart()
-  let result = parser.ParseCommandLine argv
-  timer.Stop()
+  try
+    let result = parser.ParseCommandLine argv
 
-  debug (sprintf "parser.ParseCommandLine = %d" timer.ElapsedMilliseconds)
+    isDebug <- result.Contains Parser.Debug
 
-  timer.Restart()
+    let timer = Diagnostics.Stopwatch.StartNew()
 
-  if result.Contains Parser.Arguments.Get_Books then
-    r |> Workflow.getBooks
+    if result.Contains Parser.Arguments.Get_Books then
+      r |> Workflow.getBooks
 
-  if result.Contains Parser.Arguments.Get_Logs_By_Book then
-    r |> Workflow.getLastReadingLogsByBook
+    if result.Contains Parser.Arguments.Get_Logs_By_Book then
+      r |> Workflow.getLastReadingLogsByBook
 
-  if result.Contains Parser.Arguments.Create_Book then
-    (w, bookFolder) ||> Workflow.createBook
+    if result.Contains Parser.Arguments.Create_Book then
+      (w, bookFolder) ||> Workflow.createBook
 
-  if result.Contains Parser.Arguments.Log_Reading then
-    (r, w) ||> Workflow.logReading
+    if result.Contains Parser.Arguments.Log_Reading then
+      (r, w) ||> Workflow.logReading
 
-  if result.Contains Parser.Arguments.Continue_Last_Reading then
-    r |> Workflow.continueLastReading
+    if result.Contains Parser.Arguments.Continue_Last_Reading then
+      r |> Workflow.continueLastReading
 
-  timer.Stop()
+    timer.Stop()
 
-  debug (sprintf "workflow = %d" timer.ElapsedMilliseconds)
-
-  0
+    debug (sprintf "workflow ran in %d ms" timer.ElapsedMilliseconds)
+    0
+  with :? ArguParseException as e ->
+    printf "%s" e.Message
+    1
