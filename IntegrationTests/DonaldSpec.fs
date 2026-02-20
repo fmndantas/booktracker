878d53ab8966c833efc60f78f5bb254f44ff841e
Fernando Dantas <fernandodantas1996@gmail.com>
2026-02-20 13:07:10 -0300

    test: added rollback learning test

--- IntegrationTests/DonaldSpec.fs
+++ IntegrationTests/DonaldSpec.fs
@@ -1,50 +1,41 @@
 module IntegrationTests.DonaldSpec
 
-open System.Data.SQLite
-open Donald
 open System
 
+open Donald
+
 open Expecto
+open Expecto.Flip.Expect
 
-let printDebug (message: string) =
-  printfn "[DEBUG - %s]: %s" (DateTime.UtcNow.ToString "O") message
+open App
 
-type Mark = {
-  Start: string -> unit
-  End: string -> unit
-}
+let countBooks conn =
+  conn
+  |> Db.newCommand "select count(*) as cnt from book"
+  |> Db.querySingle (fun rd -> rd.ReadInt64 "cnt")
+  |> Option.get
 
-let createMark () =
-  let timer = Diagnostics.Stopwatch.StartNew()
+let ``test rollback`` =
+  "test rollback",
+  fun (conn: Context.BooktrackerConnection) ->
+    let tran = conn.TryBeginTransaction()
 
-  let startMark =
-    fun message ->
-      printDebug (sprintf "starting: [%s]" message)
-      timer.Restart()
+    tran
+    |> Db.newCommandForTransaction "insert into book (title, modified) values (@title, @now)"
+    |> Db.execMany (
+      [
+        "title", Utils.random5String () |> sqlString
+        "now", DateTime.UtcNow |> sqlDateTime
+      ]
+      |> List.replicate 10
+    )
 
-  let endMark =
-    fun message -> printDebug (sprintf "ending: [%s]. elapsed: %d ms" message timer.ElapsedMilliseconds)
+    // This test passes if this line is discommented
+    // This happens because rollback undoes book insertions
+    // tran.TryRollback()
 
-  { Start = startMark; End = endMark }
+    conn |> countBooks |> equal "wrong count" 0
 
-let xp =
-  testCase "xp"
-  <| fun () ->
-    let path = __SOURCE_DIRECTORY__ + "/../booktracker.db"
-    let conn = new SQLiteConnection $"Data Source={path};Version=3"
-    let sql = "select * from book"
-
-    let mark = createMark()
-
-    mark.Start "query"
-    let titles = conn |> Db.newCommand sql |> Db.query (fun rd -> rd.ReadString "title")
-    mark.End "query"
-
-    mark.Start "query 2"
-    let _ = conn |> Db.newCommand sql |> Db.query (fun rd -> rd.ReadString "title")
-    mark.End "query 2"
-
-    printfn "%A" titles
-
-[<Tests>]
-let donaldSpec = testList "donald" [ xp ]
+[<PTests>]
+let DonaldSpec =
+  testList "donald" [ yield! testFixture Utils.testFixture [ ``test rollback`` ] ]
