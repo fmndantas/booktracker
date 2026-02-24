45751c5a296895d9591b181e753c2ffc52241e9d
Fernando Dantas <fernandodantas1996@gmail.com>
2026-02-23 22:19:55 -0300

    feat: added hook crud option to parser

--- App/Parser.fs
+++ App/Parser.fs
@@ -4,6 +4,7 @@ open Argu
 
 type Arguments =
   | [<AltCommandLine("-b")>] Book_Crud
+  | [<AltCommandLine("-h")>] Hook_Crud
   | [<AltCommandLine("-gl")>] Get_Logs_By_Book
   | [<AltCommandLine("-l")>] Log_Reading
   | [<AltCommandLine("-c")>] Continue_Last_Reading
@@ -13,7 +14,8 @@ type Arguments =
     member s.Usage =
       match s with
       | Book_Crud -> "access book crud"
+      | Hook_Crud -> "access hook crud"
       | Get_Logs_By_Book -> "show logs by book"
       | Log_Reading -> "log reading for a book"
-      | Continue_Last_Reading -> "continue last reading"      
+      | Continue_Last_Reading -> "continue last reading"
       | Debug -> "run application in debug mode"
